f0fee4e66bba3b29781e15489650182e5c601b1c
MrBacon470 <72811949+MrBacon470@users.noreply.github.com>
2020-11-14 11:23:57 -0600

    Cursors Added

--- main.js
+++ main.js
@@ -22,9 +22,23 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 let power = 0
+let cursors = 0;
 
 function powerClick(number)
 {
     power = power + number;
     document.getElementById("click").innerHTML = power;
 }
+
+function buyCursor(){
+    var cursorCost = Math.floor(10 * Math.pow(1.1,cursors));     //works out the cost of this cursor
+    if(power >= cursorCost){                                   //checks that the player can afford the cursor
+        cursors = cursors + 1;                                   //increases number of cursors
+    	power = power - cursorCost;                          //removes the power spent
+        document.getElementById('cursors').innerHTML = cursors;  //updates the number of cursors for the user
+        document.getElementById('power').innerHTML = power;  //updates the number of cookies for the user
+    };
+    var nextCost = Math.floor(10 * Math.pow(1.1,cursors));       //works out the cost of the next cursor
+    document.getElementById('cursorCost').innerHTML = nextCost;  //updates the cursor cost for the user
+};
+
